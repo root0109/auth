27e44184b0dd9c070b62aec522b4eb5382f263de
Vaibhav Singh <vaibhav.singh@gupshup.io>
2018-09-29 14:56:00 +0530

    second version of auth with api-doc and tests.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # auth
 Yet another Oauth system
 http://www.zakariaamine.com/2018-01-27/using-oauth2-in-spring
+https://www.oauth.com/oauth2-servers/token-introspection-endpoint/
+http://www.zakariaamine.com/2018-01-27/using-oauth2-in-spring
+https://dzone.com/articles/secure-spring-rest-with-spring-security-and-oauth2
+https://dzone.com/articles/how-to-implement-java-oauth-20-to-sign-in-with-git-1
--- src/main/java/io/zaprit/auth/config/ResourceServerConfiguration.java
+++ src/main/java/io/zaprit/auth/config/ResourceServerConfiguration.java
@@ -27,12 +27,7 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 	@Override
 	public void configure(HttpSecurity http) throws Exception
 	{
-		/*
-		 * http.requestMatchers().antMatchers(SECURED_PATTERN).and().authorizeRequests()
-		 * .antMatchers(HttpMethod.POST, SECURED_PATTERN)
-		 * .access(SECURED_WRITE_SCOPE).anyRequest().access(SECURED_READ_SCOPE);
-		 */
-
-		http.authorizeRequests().antMatchers(SECURED_PATTERN).access(SECURED_WRITE_SCOPE).anyRequest().access(SECURED_READ_SCOPE);
+		http.requestMatchers().antMatchers(SECURED_PATTERN).and().authorizeRequests().antMatchers(SECURED_PATTERN).access(SECURED_WRITE_SCOPE)
+				.anyRequest().access(SECURED_READ_SCOPE);
 	}
 }
